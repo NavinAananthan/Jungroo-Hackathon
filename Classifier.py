439c15dbe3a7e318bfcd19b191982c99c137488b
Navin Aananthan K R <81963819+NavinAananthan@users.noreply.github.com>
2023-03-19 19:34:05 +0530

    classifier

--- Classifier.py
+++ Classifier.py
@@ -1,31 +1,50 @@
 import numpy as np
-from sklearn.datasets import fetch_openml
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
 import cv2
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 
 # Load the EMNIST dataset
-emnist = fetch_openml('emnist', cache=True)
+ds_train, ds_test = tfds.load('emnist', split=['train', 'test'], shuffle_files=True, as_supervised=True)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(emnist.data, emnist.target, test_size=0.2)
+# Preprocess the data
+def preprocess_image(image, label):
+    image = tf.cast(image, tf.float32) / 255.0
+    image = tf.expand_dims(image, axis=-1)
+    label = tf.one_hot(label, depth=62)
+    return image, label
 
-# Train an SVM classifier on the training data
-clf = SVC(kernel='linear')
-clf.fit(X_train, y_train)
+ds_train = ds_train.map(preprocess_image)
+ds_train = ds_train.batch(32)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
-# Predict the labels of the test data
-y_pred = clf.predict(X_test)
+ds_test = ds_test.map(preprocess_image)
+ds_test = ds_test.batch(32)
+ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
 
-# Calculate the accuracy of the classifier
-accuracy = np.mean(y_pred == y_test)
-print('Accuracy:', accuracy)
+# Define the model architecture
+model = keras.Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+    MaxPooling2D(pool_size=(2,2)),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(pool_size=(2,2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(62, activation='softmax')
+])
 
-# Save the trained model as an XML file
-svm_file = 'char_classifier.xml'
-svm = cv2.ml.SVM_create()
-svm.setType(cv2.ml.SVM_C_SVC)
-svm.setKernel(cv2.ml.SVM_LINEAR)
-svm.setTermCriteria((cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6))
-svm.train(np.float32(X_train), cv2.ml.ROW_SAMPLE, y_train)
-svm.save(svm_file)
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+history = model.fit(ds_train, epochs=10, validation_data=ds_test)
+
+# Evaluate the model on the test set
+test_loss, test_acc = model.evaluate(ds_test)
+print('Test accuracy:', test_acc)
+
+# Save the model
+model.save('char_classifier.h5')
